0755fbceccf20d8696acea874a6a713d33907bee
RohitMaskargit <136835218+RohitMaskargit@users.noreply.github.com>
2023-08-19 16:04:28 +0530

    Add files via upload

---
+++ Rectangle.java
@@ -0,0 +1,15 @@
+class Rectangle
+{
+	void area(int a , int b)
+	{
+		System.out.println(a*b);
+	}
+
+
+   public static void main (String [] args)
+   {
+	Rectangle rec = new Rectangle();
+	rec.area(10,20);
+	
+   }
+}
